b402ab64e3d74a35a246b85f252ed6c45700948b
Sangram Pattnaik <sangrampattnaik09@gmail.com>
2021-03-11 16:11:31 +0530

    authorization implementing

--- ninja_api/api.py
+++ ninja_api/api.py
@@ -2,12 +2,36 @@ from ninja import NinjaAPI, Router, Schema
 from ninja.orm import create_schema
 from django.http import JsonResponse
 from exam.models import Questions, Standard, Student, User
+from django.contrib.auth import authenticate
 from django.shortcuts import get_object_or_404
 from rest_framework.serializers import ModelSerializer
+from django.conf import settings
+import jwt
+from ninja.security import HttpBearer
+class GlobalAuth(HttpBearer):
+    openapi_scheme = "sdfnvl"
+    def authenticate(self, request, token):
+        try:
+            payload = jwt.decode(token, settings.SECRET_KEY, ["HS256"])
+            print(payload)
+            return payload
+        except jwt.exceptions.DecodeError:
+            raise AuthorizationFailed("authorization failed")
+        except jwt.exceptions.InvalidSignatureError:
+            raise AuthorizationFailed("authorization failed")
+        except jwt.exceptions.ExpiredSignatureError:
+            raise AuthorizationFailed("token has been expired")
+        except Person.DoesNotExist:
+            raise AuthorizationFailed("authorization failed.no data found")
+        except jwt.exceptions.InvalidTokenError:
+            raise AuthorizationFailed("authorization failed")
 
 class StandardSchema(Schema):
     class_name:str
 
+class LoginSchema(Schema):
+    username:str
+    password:str
 
 class StandardSerializer(ModelSerializer):
     class Meta:
@@ -18,6 +42,7 @@ router = Router()
 
 @router.get("/standard",tags=['standard'],summary="get list of standards")
 def standard_get(request):
+    print(request.auth)
     standards = Standard.objects.all()
     standards_serilaizer = StandardSerializer(standards,many=True).data
     return JsonResponse({"status":"success","data":standards_serilaizer},status=200)
@@ -58,5 +83,18 @@ def standard_update_particular(request,standard_id,body:StandardSchema):
 @router.delete("/standard/{standard_id}",tags=['standard'],summary="delete a standard")
 def standard_delete_particular(request,standard_id):
     std = get_object_or_404(Standard,id=standard_id).delete()
-    return JsonResponse({"status":"success","msg":"standard deleted"},status=201)
+    return JsonResponse({"status":"success","msg":"standard deleted"},status=200)
+
+
+@router.post("/login",tags=['login'],summary="get login token by username and password",auth=None)
+def login(request,body:LoginSchema):
+    body = body.dict()
+    user = authenticate(username=body['username'],password=body['password'])
+    if user:
+        payload = {"id":user.id,"class_name":user.student_user.standard.class_name,"full_name":user.student_user.full_name}
+        token = jwt.encode(payload,settings.SECRET_KEY,'HS256').decode()
+        return JsonResponse({"token":token,"status":"success","msg":"login successfull"},status=200)
+    else:
+        return JsonResponse({"status":"failed","msg":"incorrect username or password"},status=400)
+
 
--- student_examination/urls.py
+++ student_examination/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
 from ninja import NinjaAPI
-from ninja_api.api import router
+from ninja_api.api import router,GlobalAuth
 from ninja_api.api2 import router as student_router
-api = NinjaAPI()
+api = NinjaAPI(version="1.0.0",title="Student MCQ Exam API",description="Students for Standard 1 to 10",auth=GlobalAuth())
 api.add_router("",router)
 api.add_router("",student_router)
 
